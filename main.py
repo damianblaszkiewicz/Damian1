4a0f2d7cd6902838341478e34f64358baf79afe1
Student <student@it.uekat.local>
2024-01-14 10:34:51 +0100

    zmiany w projekcie

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+from flask import Flask
+from flask_restful import Resource, Api
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+app = Flask(__name__)
+api = Api(app)
+
+class PeopleCounter(Resource):
+    def get(self):
+        # load image
+        image = cv2.imread('zdjecie1.jpg')
+        image = cv2.resize(image, (700, 400))
+
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'peopleCount': len(rects)}
+
+
+api.add_resource(PeopleCounter, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
